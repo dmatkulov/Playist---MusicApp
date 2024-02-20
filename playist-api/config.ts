92f68e91a2253b82970cb2398560ecf79f672e25
Dilshad Matkulov <dilshadmatkulov@gmail.com>
2024-02-20 21:19:07 +0600

    created path for images, added seed script to package.json

--- playist-api/config.ts
+++ playist-api/config.ts
@@ -5,6 +5,7 @@ const rootPath = __dirname;
 const config = {
   rootPath,
   publicPath: path.join(rootPath, 'public'),
+  fixturesPath: path.join(rootPath, 'fixtures', 'images'),
   mongoose: {
     db: 'mongodb://localhost/playist',
   },
